eb0b96060d34233ef22efaad431a922b053cddf1
Om Kamath <omganesh402@gmail.com>
2022-12-06 08:39:15 +0530

    Added kaleido engine

--- web.py
+++ web.py
@@ -77,7 +77,7 @@ with st.spinner("Crunching the data..."):
             df["datetime"] = pd.to_datetime(df.index)
             df["year"]=df["datetime"].dt.year
             fig = px.line(df, x="datetime",y=["Close","Mavg"])
-            fig.write_image("images/fig1.png")
+            fig.write_image("images/fig1.png",engine="kaleido")
             c1.plotly_chart(fig,use_container_width=True)
             c1.markdown("### Company Info")
             c1.write(info["longBusinessSummary"])
